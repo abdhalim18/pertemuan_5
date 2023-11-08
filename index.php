db7bd753aafc0fb6c5d20eacd499cf230cc654d0
abdhalim18 <mabdulhalim257@gmail.com>
2023-11-08 23:55:35 +0700

    tugas

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$pemasukan = 50000000;
+$hutangA = 16500000;
+$bungaA = 0.05;
+$hutangB = 9500000;
+$bungaB = 0.045;
+
+// jumlah bungah hutang
+$totalBungaA = $hutangA * $bungaA;
+$totalBungaB = $hutangB * $bungaB;
+$totalBungaHutang = $totalBungaA + $totalBungaB;
+
+// Totalab hutang
+$totalHutang = $hutangA + $hutangB;
+
+// Hitung sisa uang
+$sisaUang = $pemasukan - $totalHutang;
+
+// Tampilkan hasil 
+echo "Hasil perhitungan <br>";
+echo "Sisa uang: " . $sisaUang . "<br>";
+echo "Total bunga: " . $totalBungaHutang . "<br>";
+echo "Total hutang: " . $totalHutang . "<br>";
+?>
